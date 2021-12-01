f18b8f8a9d2f9b6257cd471c47b67c33f28049ec
Alexey Troyanovskiy <shtyr111@gmail.com>
2021-12-01 18:40:24 +0300

    commit all

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,11 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+    @Override
+    public String toString() {
+        return "\nНеважно, сколько людей умирает, всё равно всё остаётся по-прежнему. Чак Паланик";
+    }
+}
